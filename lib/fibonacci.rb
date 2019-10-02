adc787d38db3b59f0dfb0ae7549e69f99e41207b
eldenhowes <williameldenhowes@gmail.com>
2019-10-02 10:01:46 +0100

    Refactored further using two private methods - one to return the fib_number and one to store the fib_number in the private hash

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,20 +3,10 @@ def fibonacci(number)
         add_fib_number(number, 1)
         return 1
     end
-    if FIB_NUMBERS.include?(number -1) 
-        fib_minus_1 = FIB_NUMBERS[number -1]
-    else
-        fib_minus_1 = fibonacci(number -1)
-    end
 
-    if FIB_NUMBERS.include?(number -2)
-        fib_minus_2 = FIB_NUMBERS[number -2]
-    else
-        fib_minus_2 = fibonacci(number -2)
-    end 
-    fib_number = fib_minus_1 + fib_minus_2
-    add_fib_number(number, fib_number)
-    fib_number
+    current_fib_number = fib_number(number -1) + fib_number(number -2)
+    add_fib_number(number, current_fib_number)
+    current_fib_number
 end
 
 
@@ -25,4 +15,12 @@ FIB_NUMBERS = Hash.new
 
 def add_fib_number(number, fib_number)
     FIB_NUMBERS[number] = fib_number
+end
+
+def fib_number(number)
+    if FIB_NUMBERS.include?(number)
+        FIB_NUMBERS[number]
+    else
+        fibonacci(number)
+    end
 end
