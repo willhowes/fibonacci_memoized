33e6f56dfa075a168ca4cee35aa368f8057eb230
eldenhowes <williameldenhowes@gmail.com>
2019-10-02 09:02:24 +0100

    Correctly returns 5 for an input of 5

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,4 +1,5 @@
 def fibonacci(number)
     return 1 if number <= 2
-    2
+    return number -1 if number == 3
+    number
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -10,4 +10,8 @@ describe '#fibonacci' do
     it 'returns 2 for an input of 3' do
         expect(fibonacci(3)).to eq(2)
     end
+    
+    it 'returns 5 for an input of 5' do
+        expect(fibonacci(5)).to eq(5)
+    end
 end
